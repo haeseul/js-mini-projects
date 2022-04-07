6c04749806e08fdf741cf67aa0eab9ff391b70a8
haeseul <ksw06205@naver.com>
2022-04-07 15:03:25 +0900

    3D 캐러셀 구현

---
+++ case13_3d_carousel/script.js
@@ -0,0 +1,28 @@
+;(function () {
+  'use strict'
+
+  const get = (target) => {
+    return document.querySelector(target)
+  }
+
+  const cellCount = 6
+  let selectIndex = 0 // carousel의 첫번째부터 보여주기
+  const prevButton = get('.prev_button')
+  const nextButton = get('.next_button')
+  const carousel = get('.carousel')
+
+  const rotateCarousel = () => {
+    const angle = (selectIndex / cellCount) * -360  // item들과 반대 값으로 rotate 해줘야 함
+    carousel.style.transform = `translateZ(-346px) rotateY(${angle}deg)`
+  }
+
+  prevButton.addEventListener('click', () => {
+    selectIndex--
+    rotateCarousel()
+  })
+
+  nextButton.addEventListener('click', () => {
+    selectIndex++
+    rotateCarousel()
+  })
+})()
